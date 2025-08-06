1a7ccbe2a32eb5c73bb220639201bd6127ac0e11
AtlisTechnologies <dave@atlistechnologies.com>
2025-08-05 19:30:55 -0600

    Update atlis.sql

--- _SQL/atlis.sql
+++ _SQL/atlis.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Aug 06, 2025 at 03:06 AM
+-- Generation Time: Aug 06, 2025 at 03:30 AM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
