e1a3364f4c4d3445c1b675466f9ed972070edf4e
Atlis Technologies <dave@atlistechnologies.com>
2025-08-14 16:41:32 -0600

    Refactor project creation form

--- module/project/functions/create.php
+++ module/project/functions/create.php
@@ -8,13 +8,23 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
   $name = $_POST['name'] ?? '';
   $status = $_POST['status'] ?? null;
   $description = $_POST['description'] ?? null;
+  $requirements = $_POST['requirements'] ?? null;
+  $specifications = $_POST['specifications'] ?? null;
+  $start_date = $_POST['start_date'] ?? null;
+  $agency_id = $_POST['agency_id'] ?? null;
+  $division_id = $_POST['division_id'] ?? null;
 
-  $stmt = $pdo->prepare('INSERT INTO module_projects (user_id, user_updated, name, status, description) VALUES (:uid, :uid, :name, :status, :description)');
+  $stmt = $pdo->prepare('INSERT INTO module_projects (user_id, user_updated, agency_id, division_id, name, description, requirements, specifications, status, start_date) VALUES (:uid, :uid, :agency_id, :division_id, :name, :description, :requirements, :specifications, :status, :start_date)');
   $stmt->execute([
     ':uid' => $this_user_id,
+    ':agency_id' => $agency_id,
+    ':division_id' => $division_id,
     ':name' => $name,
+    ':description' => $description,
+    ':requirements' => $requirements,
+    ':specifications' => $specifications,
     ':status' => $status,
-    ':description' => $description
+    ':start_date' => $start_date
   ]);
   $id = $pdo->lastInsertId();
 
@@ -26,9 +36,14 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     'CREATE',
     null,
     json_encode([
+      'agency_id' => $agency_id,
+      'division_id' => $division_id,
       'name' => $name,
+      'description' => $description,
+      'requirements' => $requirements,
+      'specifications' => $specifications,
       'status' => $status,
-      'description' => $description
+      'start_date' => $start_date
     ]),
     'Created project'
   );
