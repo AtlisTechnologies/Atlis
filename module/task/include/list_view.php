52b8aab6174ce6f3085ce498ff447f15044112d4
Atlis Technologies <dave@atlistechnologies.com>
2025-08-14 22:28:32 -0600

    Add priority color badges to task list

--- module/task/include/list_view.php
+++ module/task/include/list_view.php
@@ -23,7 +23,11 @@
               <span class="badge-label"><?php echo h($task['status_label'] ?? ''); ?></span>
             </span>
           </td>
-          <td><?php echo h($task['priority_label'] ?? ''); ?></td>
+          <td>
+            <span class="badge badge-phoenix fs-10 badge-phoenix-<?php echo h($task['priority_color'] ?? ''); ?>">
+              <span class="badge-label"><?php echo h($task['priority_label'] ?? ''); ?></span>
+            </span>
+          </td>
           <td><a href="index.php?action=edit&amp;id=<?php echo (int)($task['id'] ?? 0); ?>" class="btn btn-sm btn-link">Edit</a></td>
         </tr>
       <?php endforeach; ?>
