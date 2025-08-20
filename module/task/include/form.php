4dcc250fa254a02b1e9766012bf2a89f7664389a
Atlis Technologies <dave@atlistechnologies.com>
2025-08-20 00:02:17 -0600

    Add task edit modal and expanded form

--- module/task/include/form.php
+++ module/task/include/form.php
@@ -3,7 +3,7 @@
 ?>
 <?php $isEdit = !empty($task['id']); ?>
 <?php if (!empty($isModal)): ?>
-<form class="row g-3" method="post" action="index.php?action=save">
+<form id="taskForm" class="row g-3" method="post" action="index.php?action=save">
   <input type="hidden" name="id" value="<?php echo h($task['id'] ?? ''); ?>">
   <div class="modal-header">
     <h5 class="modal-title"><?php echo $isEdit ? 'Edit Task' : 'Create Task'; ?></h5>
@@ -17,11 +17,30 @@
           <label for="taskName">Task name</label>
         </div>
       </div>
+      <div class="col-12">
+        <div class="form-floating">
+          <textarea class="form-control" id="taskDescription" name="description" placeholder="Description" style="height:100px"><?php echo h($task['description'] ?? ''); ?></textarea>
+          <label for="taskDescription">Description</label>
+        </div>
+      </div>
+      <div class="col-12">
+        <div class="form-floating">
+          <textarea class="form-control" id="taskRequirements" name="requirements" placeholder="Requirements" style="height:100px"><?php echo h($task['requirements'] ?? ''); ?></textarea>
+          <label for="taskRequirements">Requirements</label>
+        </div>
+      </div>
+      <div class="col-12">
+        <div class="form-floating">
+          <textarea class="form-control" id="taskSpecifications" name="specifications" placeholder="Specifications" style="height:100px"><?php echo h($task['specifications'] ?? ''); ?></textarea>
+          <label for="taskSpecifications">Specifications</label>
+        </div>
+      </div>
       <div class="col-sm-6 col-md-4">
         <div class="form-floating">
           <select class="form-select" id="taskStatus" name="status" required>
             <?php foreach ($statusMap as $s): ?>
-              <option value="<?php echo $s['id']; ?>" data-color="<?php echo h($s['color_class']); ?>" <?php if (($task['status'] ?? '') == $s['id']) echo 'selected'; ?>><?php echo h($s['label']); ?></option>
+              <?php $sel = (($task['status'] ?? null) == $s['id']) || (empty($task['status']) && !empty($s['is_default'])); ?>
+              <option value="<?php echo $s['id']; ?>" data-color="<?php echo h($s['color_class']); ?>" <?php echo $sel ? 'selected' : ''; ?>><?php echo h($s['label']); ?></option>
             <?php endforeach; ?>
           </select>
           <label for="taskStatus">Status</label>
@@ -31,7 +50,8 @@
         <div class="form-floating">
           <select class="form-select" id="taskPriority" name="priority" required>
             <?php foreach ($priorityMap as $p): ?>
-              <option value="<?php echo $p['id']; ?>" data-color="<?php echo h($p['color_class']); ?>" <?php if (($task['priority'] ?? '') == $p['id']) echo 'selected'; ?>><?php echo h($p['label']); ?></option>
+              <?php $sel = (($task['priority'] ?? null) == $p['id']) || (empty($task['priority']) && !empty($p['is_default'])); ?>
+              <option value="<?php echo $p['id']; ?>" data-color="<?php echo h($p['color_class']); ?>" <?php echo $sel ? 'selected' : ''; ?>><?php echo h($p['label']); ?></option>
             <?php endforeach; ?>
           </select>
           <label for="taskPriority">Priority</label>
@@ -78,6 +98,48 @@
               <option value="<?php echo $u['id']; ?>" <?php if (in_array($u['id'], $assignedUsers)) echo 'selected'; ?>><?php echo h($u['email']); ?></option>
             <?php endforeach; ?>
           </select>
+        </div>
+      </div>
+      <div class="col-sm-6 col-md-4">
+        <div class="flatpickr-input-container">
+          <div class="form-floating">
+            <input class="form-control datetimepicker" id="startDate" type="text" name="start_date" placeholder="Start date" value="<?php echo h($task['start_date'] ?? ''); ?>" data-options='{"disableMobile":true}'>
+            <label class="ps-6" for="startDate">Start date</label><span class="uil uil-calendar-alt flatpickr-icon text-body-tertiary"></span>
+          </div>
+        </div>
+      </div>
+      <div class="col-sm-6 col-md-4">
+        <div class="flatpickr-input-container">
+          <div class="form-floating">
+            <input class="form-control datetimepicker" id="dueDate" type="text" name="due_date" placeholder="Due date" value="<?php echo h($task['due_date'] ?? ''); ?>" data-options='{"disableMobile":true}'>
+            <label class="ps-6" for="dueDate">Due date</label><span class="uil uil-calendar-alt flatpickr-icon text-body-tertiary"></span>
+          </div>
+        </div>
+      </div>
+      <div class="col-sm-6 col-md-4">
+        <div class="flatpickr-input-container">
+          <div class="form-floating">
+            <input class="form-control datetimepicker" id="completeDate" type="text" name="complete_date" placeholder="Complete date" value="<?php echo h($task['complete_date'] ?? ''); ?>" data-options='{"disableMobile":true}'>
+            <label class="ps-6" for="completeDate">Complete date</label><span class="uil uil-calendar-alt flatpickr-icon text-body-tertiary"></span>
+          </div>
+        </div>
+      </div>
+      <div class="col-sm-6 col-md-4">
+        <div class="form-check form-switch mt-4">
+          <input class="form-check-input" id="completedCheck" type="checkbox" name="completed" value="1" <?php if (!empty($task['completed'])) echo 'checked'; ?>>
+          <label class="form-check-label" for="completedCheck">Completed</label>
+        </div>
+      </div>
+      <div class="col-sm-6 col-md-4">
+        <div class="form-floating">
+          <input class="form-control" id="progressPercent" type="number" name="progress_percent" min="0" max="100" placeholder="Progress %" value="<?php echo h($task['progress_percent'] ?? ''); ?>">
+          <label for="progressPercent">Progress %</label>
+        </div>
+      </div>
+      <div class="col-12">
+        <div class="form-floating">
+          <textarea class="form-control" id="taskMemo" name="memo" placeholder="Memo" style="height:100px"><?php echo h($task['memo'] ?? ''); ?></textarea>
+          <label for="taskMemo">Memo</label>
         </div>
       </div>
     </div>
@@ -97,7 +159,7 @@
 <h2 class="mb-4"><?php echo $isEdit ? 'Edit Task' : 'Create Task'; ?></h2>
 <div class="row">
   <div class="col-xl-9">
-    <form class="row g-3 mb-6" method="post" action="index.php?action=save">
+    <form id="taskForm" class="row g-3 mb-6" method="post" action="index.php?action=save">
       <input type="hidden" name="id" value="<?php echo h($task['id'] ?? ''); ?>">
       <div class="col-12">
         <div class="form-floating">
@@ -105,11 +167,30 @@
           <label for="taskName">Task name</label>
         </div>
       </div>
+      <div class="col-12">
+        <div class="form-floating">
+          <textarea class="form-control" id="taskDescription" name="description" placeholder="Description" style="height:100px"><?php echo h($task['description'] ?? ''); ?></textarea>
+          <label for="taskDescription">Description</label>
+        </div>
+      </div>
+      <div class="col-12">
+        <div class="form-floating">
+          <textarea class="form-control" id="taskRequirements" name="requirements" placeholder="Requirements" style="height:100px"><?php echo h($task['requirements'] ?? ''); ?></textarea>
+          <label for="taskRequirements">Requirements</label>
+        </div>
+      </div>
+      <div class="col-12">
+        <div class="form-floating">
+          <textarea class="form-control" id="taskSpecifications" name="specifications" placeholder="Specifications" style="height:100px"><?php echo h($task['specifications'] ?? ''); ?></textarea>
+          <label for="taskSpecifications">Specifications</label>
+        </div>
+      </div>
       <div class="col-sm-6 col-md-4">
         <div class="form-floating">
           <select class="form-select" id="taskStatus" name="status" required>
             <?php foreach ($statusMap as $s): ?>
-              <option value="<?php echo $s['id']; ?>" data-color="<?php echo h($s['color_class']); ?>" <?php if (($task['status'] ?? '') == $s['id']) echo 'selected'; ?>><?php echo h($s['label']); ?></option>
+              <?php $sel = (($task['status'] ?? null) == $s['id']) || (empty($task['status']) && !empty($s['is_default'])); ?>
+              <option value="<?php echo $s['id']; ?>" data-color="<?php echo h($s['color_class']); ?>" <?php echo $sel ? 'selected' : ''; ?>><?php echo h($s['label']); ?></option>
             <?php endforeach; ?>
           </select>
           <label for="taskStatus">Status</label>
@@ -119,7 +200,8 @@
         <div class="form-floating">
           <select class="form-select" id="taskPriority" name="priority" required>
             <?php foreach ($priorityMap as $p): ?>
-              <option value="<?php echo $p['id']; ?>" data-color="<?php echo h($p['color_class']); ?>" <?php if (($task['priority'] ?? '') == $p['id']) echo 'selected'; ?>><?php echo h($p['label']); ?></option>
+              <?php $sel = (($task['priority'] ?? null) == $p['id']) || (empty($task['priority']) && !empty($p['is_default'])); ?>
+              <option value="<?php echo $p['id']; ?>" data-color="<?php echo h($p['color_class']); ?>" <?php echo $sel ? 'selected' : ''; ?>><?php echo h($p['label']); ?></option>
             <?php endforeach; ?>
           </select>
           <label for="taskPriority">Priority</label>
@@ -166,6 +248,48 @@
               <option value="<?php echo $u['id']; ?>" <?php if (in_array($u['id'], $assignedUsers)) echo 'selected'; ?>><?php echo h($u['email']); ?></option>
             <?php endforeach; ?>
           </select>
+        </div>
+      </div>
+      <div class="col-sm-6 col-md-4">
+        <div class="flatpickr-input-container">
+          <div class="form-floating">
+            <input class="form-control datetimepicker" id="startDate" type="text" name="start_date" placeholder="Start date" value="<?php echo h($task['start_date'] ?? ''); ?>" data-options='{"disableMobile":true}'>
+            <label class="ps-6" for="startDate">Start date</label><span class="uil uil-calendar-alt flatpickr-icon text-body-tertiary"></span>
+          </div>
+        </div>
+      </div>
+      <div class="col-sm-6 col-md-4">
+        <div class="flatpickr-input-container">
+          <div class="form-floating">
+            <input class="form-control datetimepicker" id="dueDate" type="text" name="due_date" placeholder="Due date" value="<?php echo h($task['due_date'] ?? ''); ?>" data-options='{"disableMobile":true}'>
+            <label class="ps-6" for="dueDate">Due date</label><span class="uil uil-calendar-alt flatpickr-icon text-body-tertiary"></span>
+          </div>
+        </div>
+      </div>
+      <div class="col-sm-6 col-md-4">
+        <div class="flatpickr-input-container">
+          <div class="form-floating">
+            <input class="form-control datetimepicker" id="completeDate" type="text" name="complete_date" placeholder="Complete date" value="<?php echo h($task['complete_date'] ?? ''); ?>" data-options='{"disableMobile":true}'>
+            <label class="ps-6" for="completeDate">Complete date</label><span class="uil uil-calendar-alt flatpickr-icon text-body-tertiary"></span>
+          </div>
+        </div>
+      </div>
+      <div class="col-sm-6 col-md-4">
+        <div class="form-check form-switch mt-4">
+          <input class="form-check-input" id="completedCheck" type="checkbox" name="completed" value="1" <?php if (!empty($task['completed'])) echo 'checked'; ?>>
+          <label class="form-check-label" for="completedCheck">Completed</label>
+        </div>
+      </div>
+      <div class="col-sm-6 col-md-4">
+        <div class="form-floating">
+          <input class="form-control" id="progressPercent" type="number" name="progress_percent" min="0" max="100" placeholder="Progress %" value="<?php echo h($task['progress_percent'] ?? ''); ?>">
+          <label for="progressPercent">Progress %</label>
+        </div>
+      </div>
+      <div class="col-12">
+        <div class="form-floating">
+          <textarea class="form-control" id="taskMemo" name="memo" placeholder="Memo" style="height:100px"><?php echo h($task['memo'] ?? ''); ?></textarea>
+          <label for="taskMemo">Memo</label>
         </div>
       </div>
       <div class="col-12 gy-6">
