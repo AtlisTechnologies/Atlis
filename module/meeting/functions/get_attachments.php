2921a5add3af36f08f14bf91ef6d90c2d14f0c6a
Atlis Technologies <dave@atlistechnologies.com>
2025-08-23 17:04:59 -0600

    Simplify meeting attachments

---
+++ module/meeting/functions/get_attachments.php
@@ -0,0 +1,22 @@
+<?php
+require '../../../includes/php_header.php';
+require_permission('meeting', 'read');
+
+header('Content-Type: application/json');
+
+$meeting_id = (int)($_GET['meeting_id'] ?? 0);
+$files = [];
+if ($meeting_id) {
+    $stmt = $pdo->prepare('SELECT id, file_name, file_path FROM module_meeting_files WHERE meeting_id = ? ORDER BY id');
+    $stmt->execute([$meeting_id]);
+    while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
+        $files[] = [
+            'id' => (int)$row['id'],
+            'name' => $row['file_name'],
+            'url' => getURLDir() . ltrim($row['file_path'], '/')
+        ];
+    }
+}
+
+echo json_encode(['success' => true, 'files' => $files]);
+exit;
