c3c2b54cd8e2745a8fb364a822b00e651aad5baf
Atlis Technologies <dave@atlistechnologies.com>
2025-08-23 16:56:12 -0600

    Refactor feedback permissions and add CRUD routes

--- module/feedback/index.php
+++ module/feedback/index.php
@@ -22,12 +22,40 @@ switch ($action) {
     break;
 
   case 'save':
-    require_permission('feedback', 'save');
+    require_permission('feedback', 'create');
     require 'functions/create.php';
     break;
 
+  case 'edit':
+    require_permission('feedback', 'update');
+    $types = get_lookup_items($pdo, 'FEEDBACK_TYPE');
+    require 'functions/edit.php';
+    require '../../includes/html_header.php';
+    ?>
+    <main class="main" id="top">
+      <?php // require '../../includes/left_navigation.php'; ?>
+      <?php require '../../includes/navigation.php'; ?>
+      <div id="main_content" class="content">
+        <?php require 'include/form.php'; ?>
+        <?php require '../../includes/html_footer.php'; ?>
+      </div>
+    </main>
+    <?php require '../../includes/js_footer.php'; ?>
+    <?php
+    break;
+
+  case 'update':
+    require_permission('feedback', 'update');
+    require 'functions/update.php';
+    break;
+
+  case 'delete':
+    require_permission('feedback', 'delete');
+    require 'functions/delete.php';
+    break;
+
   case 'details':
-    require_permission('feedback', 'details');
+    require_permission('feedback', 'read');
     $types = get_lookup_items($pdo, 'FEEDBACK_TYPE');
     $typeMap = array_column($types, null, 'id');
     require 'functions/details.php';
@@ -47,7 +75,7 @@ switch ($action) {
 
   case 'list':
   default:
-    require_permission('feedback', 'list');
+    require_permission('feedback', 'read');
     $types = get_lookup_items($pdo, 'FEEDBACK_TYPE');
     $typeMap = array_column($types, null, 'id');
     require 'functions/list.php';
