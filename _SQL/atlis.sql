8565b9aa75409e2f8af6bdb46ad98af97f902433
AtlisTechnologies <dave@atlistechnologies.com>
2025-08-23 01:37:22 -0600

    Update atlis.sql

--- _SQL/atlis.sql
+++ _SQL/atlis.sql
@@ -1267,7 +1267,7 @@ CREATE TABLE `module_feedback` (
   `memo` text DEFAULT NULL,
   `title` varchar(255) DEFAULT NULL,
   `description` text DEFAULT NULL,
-  `type_id` int(11) DEFAULT NULL
+  `type` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
