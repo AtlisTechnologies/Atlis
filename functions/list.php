f6ed323dc15556e644b586f69aaf56861186c8b2
Atlis Technologies <dave@atlistechnologies.com>
2025-08-24 18:08:26 -0600

    Include link details and RBAC check for calendar

--- functions/list.php
+++ functions/list.php
@@ -7,13 +7,13 @@ $scope = $_GET['scope'] ?? 'shared';
 $currentDate = date('Y-m-d');
 if ($scope === 'mine') {
   $events = [
-    ['id' => 'm1', 'title' => 'My Task', 'start' => $currentDate . 'T09:00:00'],
-    ['id' => 'm2', 'title' => 'Personal Event', 'start' => date('Y-m-d', strtotime('+2 days')) . 'T13:00:00']
+    ['id' => 'm1', 'title' => 'My Task', 'start' => $currentDate . 'T09:00:00', 'link_module' => 'task', 'link_record_id' => 1],
+    ['id' => 'm2', 'title' => 'Personal Event', 'start' => date('Y-m-d', strtotime('+2 days')) . 'T13:00:00', 'link_module' => 'meeting', 'link_record_id' => 2]
   ];
 } else {
   $events = [
-    ['id' => 's1', 'title' => 'Team Meeting', 'start' => date('Y-m-d', strtotime('+1 day')) . 'T10:00:00'],
-    ['id' => 's2', 'title' => 'Deadline', 'start' => date('Y-m-d', strtotime('+3 days'))]
+    ['id' => 's1', 'title' => 'Team Meeting', 'start' => date('Y-m-d', strtotime('+1 day')) . 'T10:00:00', 'link_module' => 'meeting', 'link_record_id' => 10],
+    ['id' => 's2', 'title' => 'Deadline', 'start' => date('Y-m-d', strtotime('+3 days')), 'link_module' => 'project', 'link_record_id' => 20]
   ];
 }
 echo json_encode($events);
