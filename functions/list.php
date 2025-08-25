56022a9b8a2d3d398d3fc052b5e15bae293f6559
Atlis Technologies <dave@atlistechnologies.com>
2025-08-25 01:37:01 -0600

    Wrap database query logic in try/catch

--- functions/list.php
+++ functions/list.php
@@ -3,17 +3,31 @@ if (!isset($pdo)) {
   require '../includes/php_header.php';
 }
 header('Content-Type: application/json');
-$scope = $_GET['scope'] ?? 'shared';
-$currentDate = date('Y-m-d');
-if ($scope === 'mine') {
-  $events = [
-    ['id' => 'm1', 'title' => 'My Task', 'start' => $currentDate . 'T09:00:00', 'link_module' => 'task', 'link_record_id' => 1],
-    ['id' => 'm2', 'title' => 'Personal Event', 'start' => date('Y-m-d', strtotime('+2 days')) . 'T13:00:00', 'link_module' => 'meeting', 'link_record_id' => 2]
-  ];
-} else {
-  $events = [
-    ['id' => 's1', 'title' => 'Team Meeting', 'start' => date('Y-m-d', strtotime('+1 day')) . 'T10:00:00', 'link_module' => 'meeting', 'link_record_id' => 10],
-    ['id' => 's2', 'title' => 'Deadline', 'start' => date('Y-m-d', strtotime('+3 days')), 'link_module' => 'project', 'link_record_id' => 20]
-  ];
+
+try {
+  $scope = $_GET['scope'] ?? 'shared';
+  $currentDate = date('Y-m-d');
+
+  if ($scope === 'mine') {
+    $events = [
+      ['id' => 'm1', 'title' => 'My Task', 'start' => $currentDate . 'T09:00:00', 'link_module' => 'task', 'link_record_id' => 1],
+      ['id' => 'm2', 'title' => 'Personal Event', 'start' => date('Y-m-d', strtotime('+2 days')) . 'T13:00:00', 'link_module' => 'meeting', 'link_record_id' => 2]
+    ];
+  } else {
+    $events = [
+      ['id' => 's1', 'title' => 'Team Meeting', 'start' => date('Y-m-d', strtotime('+1 day')) . 'T10:00:00', 'link_module' => 'meeting', 'link_record_id' => 10],
+      ['id' => 's2', 'title' => 'Deadline', 'start' => date('Y-m-d', strtotime('+3 days')), 'link_module' => 'project', 'link_record_id' => 20]
+    ];
+  }
+
+  if (ob_get_length()) {
+    ob_clean();
+  }
+  echo json_encode($events);
+} catch (Exception $e) {
+  http_response_code(500);
+  if (ob_get_length()) {
+    ob_clean();
+  }
+  echo json_encode(['error' => $e->getMessage()]);
 }
-echo json_encode($events);
