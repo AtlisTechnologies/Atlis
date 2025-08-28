68eb9171ef379710be1d045aaa7e9a0dcaab81bd
AtlisTechnologies <dave@atlistechnologies.com>
2025-08-27 23:15:38 -0600

    Update jumbotron.php

--- includes/home/jumbotron.php
+++ includes/home/jumbotron.php
@@ -12,20 +12,7 @@ $navLinks = $stmt->fetchAll(PDO::FETCH_ASSOC);
   <p class="lead">Review recent activity and quickly jump to the modules that keep your work moving.</p>
   <hr class="my-4">
   <p>Use the navigation to explore projects, assign tasks, update contacts and monitor finances.</p>
-
-  <?php if (!empty($navLinks)): ?>
-  <div class="row g-3 mt-4">
-    <?php foreach ($navLinks as $link): ?>
-    <div class="col-6 col-md-4 col-lg-3">
-      <a class="btn btn-outline-primary w-100 text-start d-flex align-items-center" href="<?= getURLDir(); ?>admin/<?= e($link['path']); ?>">
-        <span data-feather="<?= e($link['icon'] ?: 'circle'); ?>" class="me-2"></span>
-        <span><?= e($link['title']); ?></span>
-      </a>
-      <small class="text-body-secondary d-block ms-4"><?= e($link['path']); ?></small>
-    </div>
-    <?php endforeach; ?>
-  </div>
-  <?php endif; ?>
+  
 </div>
 
 <script>
@@ -35,4 +22,3 @@ document.addEventListener('DOMContentLoaded', function() {
   }
 });
 </script>
-
